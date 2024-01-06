816416feb106ac433df4b71913dfe99abcca4f02
xoho <2651059+xoho@users.noreply.github.com>
2024-01-06 07:54:19 -0600

    try v2 endpoint

--- src/app.py
+++ src/app.py
@@ -50,7 +50,8 @@ class Twitter:
     authorize_url = "https://api.twitter.com/oauth/authorize"
     show_user_url = "https://api.twitter.com/1.1/users/show.json"
     followers_url = "https://api.twitter.com/1.1/followers/ids.json"
-    following_url = "https://api.twitter.com/1.1/friends/ids.json"
+    # following_url = "https://api.twitter.com/1.1/friends/ids.json"
+    followers_url = "https://api.twitter.com/2/users/:id/following"
     consumer_key = os.getenv("TWITTER_OAUTH2_CLIENT_ID", None)
     consumer_secret = os.getenv("TWITTER_OAUTH2_CLIENT_SECRET", None)
     redirect_uri = os.getenv("TWITTER_OAUTH_REDIRECT_URL", None)
@@ -447,10 +448,10 @@ def twitter_oauth_callback(session):
     real_token = oauth.Token(session["real_oauth_token"], session["real_oauth_token_secret"])
     real_client = oauth.Client(consumer, real_token)
 
-    real_resp, real_content = real_client.request(twcfg.following_url, "GET")
+    real_resp, real_content = real_client.request(twcfg.following_url.replace(":id", user_id), "GET")
 
     if real_resp["status"] != "200":
-        logging.error(f"Invalid response from Twitter API GET users/show: {real_resp.status} {real_content}")
+        logging.error(f"Invalid response from Twitter API GET {twcfg.followers_url}: {real_resp.status} {real_content}")
         return "Error in response from Twitter"
 
     response = json.loads(real_content.decode("utf-8"))
